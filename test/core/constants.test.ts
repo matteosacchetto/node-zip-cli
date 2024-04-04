c7fed02ad961531f826a9c202af547eb9ef09cb1
Matteo Sacchetto <56300116+matteosacchetto@users.noreply.github.com>
2024-04-04 22:54:55 +0200

    test: add test for constants

---
+++ test/core/constants.test.ts
@@ -0,0 +1,30 @@
+import assert from 'node:assert';
+import { platform } from 'node:os';
+import { join, relative } from 'node:path';
+import { describe, test } from 'node:test';
+import { fileURLToPath } from 'node:url';
+import { is_windows, preset_compression_level } from '@/core/constants';
+
+const filename = relative(
+  join(process.cwd(), 'test'),
+  fileURLToPath(import.meta.url)
+).replace('.test', '');
+
+describe(filename, async () => {
+  describe('is_windows', async () => {
+    test('windows', { skip: platform() !== 'win32' }, async (context) => {
+      assert.strictEqual(is_windows, true);
+    });
+
+    test('not windows', { skip: platform() === 'win32' }, async (context) => {
+      assert.strictEqual(is_windows, false);
+    });
+  });
+
+  describe('preset_compression_level', async () => {
+    test('default level', async () => {
+      // Used to check if this values is changed
+      assert.strictEqual(preset_compression_level, 6);
+    });
+  });
+});
