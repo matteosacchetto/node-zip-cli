dc9272ee3e6738ff543cc83dc67a54fb54ac923d
Matteo Sacchetto <56300116+matteosacchetto@users.noreply.github.com>
2024-04-05 12:02:45 +0200

    fix: add check to skip logging if list is empty

--- src/utils/broken-symlink.ts
+++ src/utils/broken-symlink.ts
@@ -3,17 +3,19 @@ import type { BrokenSymlink } from '@/types/fs';
 import chalk from 'chalk';
 
 export const log_broken_symlink = (broken_symlinks_list: BrokenSymlink[]) => {
-  logger.warning(
-    `${broken_symlinks_list.length} broken symlink${
-      broken_symlinks_list.length > 1 ? 's' : ''
-    }`
-  );
-  logger.log(
-    'The following list of symlinks point to entries not contained in the archive'
-  );
-  for (const entry of broken_symlinks_list) {
-    logger.log(
-      `${chalk.cyan(entry.cleaned_path)} -> ${chalk.red(entry.link_name)}`
+  if (broken_symlinks_list.length > 0) {
+    logger.warning(
+      `${broken_symlinks_list.length} broken symlink${
+        broken_symlinks_list.length > 1 ? 's' : ''
+      }`
     );
+    logger.log(
+      'The following list of symlinks point to entries not contained in the archive'
+    );
+    for (const entry of broken_symlinks_list) {
+      logger.log(
+        `${chalk.cyan(entry.cleaned_path)} -> ${chalk.red(entry.link_name)}`
+      );
+    }
   }
 };
--- src/utils/conflicts.ts
+++ src/utils/conflicts.ts
@@ -3,18 +3,20 @@ import type { ConflictingFsEntry } from '@/types/fs';
 import chalk from 'chalk';
 
 export const log_conflicts = (conflicting_list: ConflictingFsEntry[]) => {
-  logger.warning(
-    `${conflicting_list.length} conflicting entrie${
-      conflicting_list.length > 1 ? 's' : ''
-    }`
-  );
-  logger.log('The following list of entries conflicts with other entries');
-
-  for (const entry of conflicting_list) {
-    logger.log(
-      `${chalk.yellow(entry.conflicting_path)} -> ${chalk.green(
-        entry.conflicting_with_path
-      )}`
+  if (conflicting_list.length > 0) {
+    logger.warning(
+      `${conflicting_list.length} conflicting entr${
+        conflicting_list.length > 1 ? 'ies' : 'y'
+      }`
     );
+    logger.log('The following list of entries conflicts with other entries');
+
+    for (const entry of conflicting_list) {
+      logger.log(
+        `${chalk.yellow(entry.conflicting_path)} -> ${chalk.green(
+          entry.conflicting_with_path
+        )}`
+      );
+    }
   }
 };
