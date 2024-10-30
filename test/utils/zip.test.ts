74d99ec37765d9e979b3ff105e30bbe5b0fbda0f
Matteo Sacchetto <56300116+matteosacchetto@users.noreply.github.com>
2024-10-30 12:22:27 +0100

    test: add tests for zip utility functions

--- test/utils/zip.test.ts
+++ test/utils/zip.test.ts
@@ -2,7 +2,16 @@ import assert from 'node:assert';
 import { join, relative } from 'node:path';
 import { describe, test } from 'node:test';
 import { fileURLToPath } from 'node:url';
-import { is_symlink } from '@/utils/zip';
+import { text } from '@/utils/streams';
+import {
+  is_symlink,
+  open_read_stream,
+  open_zip_file,
+  read_entries,
+} from '@/utils/zip';
+
+const data_dir = join(process.cwd(), 'test', '_data_');
+const archives_dir = join(process.cwd(), 'test', '_archives_');
 
 const filename = relative(
   join(process.cwd(), 'test'),
@@ -24,6 +33,56 @@ describe(filename, async () => {
     test('dir', async (context) => {
       const mode = 0o40775;
       assert.ok(!is_symlink(mode));
+    });
+  });
+
+  describe('open_zip_file', async () => {
+    test('files-dir.zip', async (context) => {
+      assert.ok(open_zip_file(join(archives_dir, 'files-dir.zip')));
+    });
+
+    test('files-dir.tar', async (context) => {
+      assert.rejects(open_zip_file(join(archives_dir, 'files-dir.tar')));
+    });
+  });
+
+  describe('open_read_stream', async () => {
+    test('base.zip', async (context) => {
+      const zip = await open_zip_file(join(data_dir, 'base.zip'));
+      for await (const entry of read_entries(zip)) {
+        if (entry.fileName.endsWith('/')) {
+          continue;
+        }
+
+        const read_stream = await open_read_stream(zip, entry);
+        const data = await text(read_stream);
+
+        assert.strictEqual(data.length, 6);
+      }
+    });
+  });
+
+  describe('read_entries', async () => {
+    test('base.zip', async (context) => {
+      const zip = await open_zip_file(join(data_dir, 'base.zip'));
+      let counter = 0;
+      for await (const _ of read_entries(zip)) {
+        counter++;
+      }
+
+      assert.strictEqual(counter, 2);
+    });
+
+    test('partial base.zip', async (context) => {
+      const zip = await open_zip_file(join(data_dir, 'base.zip'));
+      const entries = read_entries(zip);
+
+      const entry = await entries[Symbol.asyncIterator]().next();
+      assert.strictEqual(entry.value!.fileName, 'a.txt');
+      await entries[Symbol.asyncIterator]().return(); // zip is now closed
+
+      const entries1 = read_entries(zip);
+      assert.rejects(entries1[Symbol.asyncIterator]().next());
     });
   });
 });
