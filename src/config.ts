e97c8a2e059d50a8490ea6ce38cf878ddb5aa0c8
Matteo Sacchetto <56300116+matteosacchetto@users.noreply.github.com>
2025-02-05 22:39:20 +0100

    fix: ensure config is populated outside rollup

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,6 @@
  *
  * @see rollup.config.mjs for details on how this is done
  */
-export const name = process.env.PKG_NAME;
-export const version = process.env.PKG_VERSION;
-export const description = process.env.PKG_DESCRIPTION;
+export const name = process.env.PKG_NAME ?? '<cmd>';
+export const version = process.env.PKG_VERSION ?? '<version>';
+export const description = process.env.PKG_DESCRIPTION ?? '<description>';
